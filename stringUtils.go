4eb0cf61463c7140c7a33c6fab395c5bb835a89b
Andrei Burdun <burdun@amazon.com>
2017-04-14 17:44:57 +0300

    Releasing v2

---
+++ stringUtils.go
@@ -0,0 +1,13 @@
+package stringUtils
+
+import (
+	"strings"
+)
+
+func Trim(str string) string {
+	return strings.Trim(str, " ")
+}
+
+func ToLower(str string) string {
+        return ToLower(str)
+}
